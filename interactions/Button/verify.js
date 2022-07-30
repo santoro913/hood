12530d782fe7d48ffddf4ad0135b753d4709a5d1
ali Mackbouk <LecointreLuc@gmail.com>
2022-07-30 12:30:01 +0200

    new

--- interactions/Button/verify.js
+++ interactions/Button/verify.js
@@ -9,8 +9,7 @@ module.exports.run = async ({ client, int }) => {
     if (int.customId !== "verify") return;
     await int.deferReply({ ephemeral: true })
     await Promise.all([
-        int.member.roles.add("986632229235064833"),
-        int.member.roles.remove("988484316680028190")
+        int.member.roles.add("1002705816140402728"),
     ]);
     return await int.editReply("Vous êtes maintenant vérifié sur le serveur !")
 };
